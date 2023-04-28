4cb86c4f7c2302884008546b2404a5748c38056e
James Millington <jamesdamillington@gmail.com>
2023-04-28 09:43:27 +0100

    finalise and create test data

--- Inputs/ClimBRA-summary.r
+++ Inputs/ClimBRA-summary.r
@@ -2,10 +2,8 @@
 #for gridded data from https://doi.org/10.57760/sciencedb.02316
 
 library(terra)
-library(zoo)  #required for as.yearmon (masks terra::time)
-library(lubridate)  #for ym()
+library(lubridate)  #for ym() and ceiling_date()
 library(logger)
-library(ncdf4)
 
 path <- "/home/james/Documents/data/"
 logfile <- "ClimBRA-summary.log"
@@ -15,9 +13,7 @@ log_appender(appender_tee(logfile))
 model <- c("EC-EARTH3")
 scenario <- c("ssp585")
 startym <- "2018-01"
-endym <- "2018-03"
-log_info(model)
-log_info(scenario)
+endym <- "2020-12"
 
 for(m in model){
   for(s in scenario){
@@ -28,31 +24,23 @@ for(m in model){
     log_info(paste0("reading ",fpin))
     dat <- rast(fpin)
     
-    dat <- subset(dat,time(dat) > ym(startym) & time(dat) <= ym(endym))
+    log_info("subset")
+    dat <- subset(dat,time(dat) >= ym(startym) & time(dat) < ceiling_date(ym(endym), unit="months"))
     
     log_info("starting summary (year-months sum)")
     datym <- tapp(dat, "yearmonths", sum)
-    #terra::time(datym) <- seq(as.yearmon(startym),as.yearmon(endym),by=1/12)
-    #terra::time(datym) <- seq(ym(startym),ym(endym),by="months")
-    #terra::time(datym, tstep="yearmonths") <- seq(ym(startym),ym(endym),by="months")
-    #names(datym) <- seq(ym(startym),ym(endym),by="months")
-    #names(datym) <- seq(as.yearmon(startym),as.yearmon(endym),by=1/12)
-    
-    fpout <- paste0(path,model,"-pr-",scenario,"-months-",startym,"-to-",endym,".nc")
+
+    #terra cannot write year-month to nc as it uses GDAL engine
+    #terra also does not preserve layer names (unless they are defined as sub-datasets)
+    #also zvals must be values (cannot be times)
+    #so best we can do is save as much info in globals (and filename) and 
+    #then set time when reading in elsewhere (e.g. moistureMap_future_cerrado.r) 
+    fpout <- paste0(path,model,"-pr-",scenario,"-totalmonth-",startym,"-to-",endym,".nc")
     log_info(paste0("writing ", fpout))
     writeCDF(datym, fpout, overwrite=T,
              varname="pr", unit="mm", 
              longname=paste0("total monthly precipitation ",startym," to ",endym))
     
-    #terra cannot write year-month to nc as it uses GDAL engine
-    #terra also does not preserve layer names (unless they are defined as sub-datasets)
-    #also zvals must be values (cannot be times)
-    #so best we can do is save as much info in globals (and filename) and then 
-  }
-}
-
-
-
     #temperature variables with mean
     for(tas in c("tasmax", "tasmin")){
       
@@ -60,27 +48,17 @@ for(m in model){
       log_info(paste0("reading ",fpin))
       dat <- rast(fpin)
       
-      dat <- subset(dat,time(dat) > ym(startym) & time(dat) <= ym(endym))
-      
+      log_info("subset")
+      dat <- subset(dat, time(dat) >= ym(startym) & time(dat) < ceiling_date(ym(endym), unit="months"))
+
       log_info("starting summary (year-months mean)")
       datym <- tapp(dat, "yearmonths", mean)
-      terra::time(datym, tstep="yearmon") <- seq(ym(startym),ym(endym),by="months")
 
-      nctime <- ncdim_def("Time","yearmon", seq(ym(startym),ym(endym),by="months"))
-      
-      fpout <- paste0(path,model,"-",tas,"-",scenario,"-months.nc" )
+      fpout <- paste0(path,model,"-",tas,"-",scenario,"-meanmonth-",startym,"-to-",endym,".nc")
       log_info(paste0("writing ", fpout))
-      writeCDF(datym, fpout, overwrite=T)
-      
-      zvals = lubridate::parse_date_time(seq(ym(startym),ym(endym),by="months"),
-                                         orders = 'ymd', tz = "UTC")
-      zvals = as.integer(zvals)
-      
-      writeCDF(datym, fpout, overwrite=T, 
+      writeCDF(datym, fpout, overwrite=T,
                varname=tas, unit="degC", 
-               longname=paste0("mean monthly ", tas), 
-               zname="Time", ncdf4::ncvar_put(nc, 'Time', zvals))
-               
+               longname=paste0("mean monthly ", tas,", ",startym," to ",endym))
     }
   }
 }
